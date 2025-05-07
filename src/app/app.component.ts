37d06a8701a925efc1880a726103971e1f9c28ea
Gustavo Braz <86533841+GustavoBrazThomaz@users.noreply.github.com>
2025-05-07 12:52:25 -0300

    feat: create home page design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppBarComponent } from './components/app-bar/app-bar.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppBarComponent, ContainerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'oral-desk';
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { TableComponent } from '../../components/table/table.component';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideCirclePlus } from '@ng-icons/lucide';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [TableComponent, NgIcon],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  viewProviders: [provideIcons({ lucideCirclePlus })],
+})
+export class HomeComponent {
+  cards = [
+    {
+      title: 'Total de Pacientes',
+      content: '128',
+      subtitle: '12 novos este mês',
+    },
+    {
+      title: 'Total de Pacientes',
+      content: '128',
+      subtitle: '12 novos este mês',
+    },
+    {
+      title: 'Total de Pacientes',
+      content: '128',
+      subtitle: '12 novos este mês',
+    },
+  ];
+
+  selectedTab = 0;
+  tabs = ['Pacientes recentes', 'Próximas consultas'];
+  tabContent = [
+    {
+      title: 'Pacientes recentes',
+      subtitle: 'Lista dos últimos pacientes cadastrados ou atendidos.',
+      buttonTitle: 'Ver todos os pacientes',
+    },
+    {
+      title: 'Próximas consultas',
+      subtitle: 'Consultas agendadas para os próximos dias.',
+      buttonTitle: 'Ver todas as consultas',
+    },
+  ];
+
+  head = ['', 'nome', 'idade'];
+  meusDados = [
+    { id: 1, nome: 'João', idade: 30 },
+    { id: 2, nome: 'Maria', idade: 25 },
+    { id: 3, nome: 'Maria', idade: 25 },
+    { id: 4, nome: 'Maria', idade: 25 },
+    { id: 5, nome: 'Maria', idade: 25 },
+  ];
+
+  selectTab(index: number) {
+    this.selectedTab = index;
+  }
+}
